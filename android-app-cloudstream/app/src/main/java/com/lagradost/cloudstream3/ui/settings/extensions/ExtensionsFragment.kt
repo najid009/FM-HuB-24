fc8282e21acdf48d5d73aceaeb5883cd00b01af8
najid009 <3.31015466e+08+najid009@users.noreply.github.com>
2026-09-19 19:00:20 +0000

    Remove in-app repository dialog

--- android-app-cloudstream/app/src/main/java/com/lagradost/cloudstream3/ui/settings/extensions/ExtensionsFragment.kt
+++ android-app-cloudstream/app/src/main/java/com/lagradost/cloudstream3/ui/settings/extensions/ExtensionsFragment.kt
@@ -1,15 +1,10 @@
 package com.lagradost.cloudstream3.ui.settings.extensions
 
-import android.content.ClipboardManager
 import android.content.Context
-import android.content.DialogInterface
 import android.os.Build
-import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.widget.LinearLayout
-import android.widget.Toast
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.isGone
 import androidx.core.view.isVisible
@@ -17,14 +12,11 @@ import androidx.core.view.marginBottom
 import androidx.core.view.marginTop
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
-import com.lagradost.cloudstream3.CommonActivity.showToast
 import com.lagradost.cloudstream3.MainActivity.Companion.afterRepositoryLoadedEvent
 import com.lagradost.cloudstream3.R
-import com.lagradost.cloudstream3.databinding.AddRepoInputBinding
 import com.lagradost.cloudstream3.databinding.FragmentExtensionsBinding
 import com.lagradost.cloudstream3.mvvm.observe
 import com.lagradost.cloudstream3.mvvm.observeNullable
-import com.lagradost.cloudstream3.plugins.RepositoryManager
 import com.lagradost.cloudstream3.ui.BaseFragment
 import com.lagradost.cloudstream3.ui.result.FOCUS_SELF
 import com.lagradost.cloudstream3.ui.result.setLinearListLayout
@@ -34,13 +26,7 @@ import com.lagradost.cloudstream3.ui.settings.Globals.isLayout
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.setSystemBarsPadding
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.setToolBarScrollFlags
 import com.lagradost.cloudstream3.ui.settings.SettingsFragment.Companion.setUpToolbar
-import com.lagradost.cloudstream3.utils.AppContextUtils.addRepositoryDialog
 import com.lagradost.cloudstream3.utils.AppContextUtils.setDefaultFocus
-import com.lagradost.cloudstream3.utils.Coroutines.ioSafe
-import com.lagradost.cloudstream3.utils.Coroutines.main
-import com.lagradost.cloudstream3.utils.UIHelper.dismissSafe
-import com.lagradost.cloudstream3.utils.UIHelper.hideProgress
-import com.lagradost.cloudstream3.utils.UIHelper.showProgress
 import com.lagradost.cloudstream3.utils.setText
 
 class ExtensionsFragment : BaseFragment<FragmentExtensionsBinding>(
@@ -220,83 +206,6 @@ class ExtensionsFragment : BaseFragment<FragmentExtensionsBinding>(
                     return true
                 }
             })
-        }
-
-
-        val addRepositoryClick = View.OnClickListener {
-            val ctx = context ?: return@OnClickListener
-            val binding = AddRepoInputBinding.inflate(LayoutInflater.from(ctx), null, false)
-            val builder =
-                AlertDialog.Builder(ctx, R.style.AlertDialogCustom)
-                    .setView(binding.root)
-
-            val dialog = builder.create()
-            dialog.show()
-            (activity?.getSystemService(Context.CLIPBOARD_SERVICE) as? ClipboardManager)?.primaryClip?.getItemAt(
-                0
-            )?.text?.toString()?.let { copiedText ->
-                if (copiedText.contains(RepoAdapter.SHAREABLE_REPO_SEPARATOR)) {
-                    // text is of format <repository name> : <repository url>
-                    val (name, url) = copiedText.split(
-                        RepoAdapter.SHAREABLE_REPO_SEPARATOR,
-                        limit = 2
-                    )
-                    binding.repoUrlInput.setText(url.trim())
-                    binding.repoNameInput.setText(name.trim())
-                } else {
-                    binding.repoUrlInput.setText(copiedText)
-                }
-            }
-
-            binding.applyBtt.setOnClickListener secondListener@{
-                val name = binding.repoNameInput.text?.toString()
-                val urlInput = binding.repoUrlInput.text?.toString()
-                if (urlInput.isNullOrEmpty()) {
-                    showToast(R.string.error_invalid_url, Toast.LENGTH_SHORT)
-                    return@secondListener
-                }
-                binding.applyBtt.showProgress()
-                ioSafe {
-                    try {
-                        val url = RepositoryManager.parseRepoUrl(urlInput)
-                        if (url.isNullOrBlank()) {
-                            showToast(R.string.error_invalid_data, Toast.LENGTH_SHORT)
-                            return@ioSafe
-                        }
-                        val repository = RepositoryManager.parseRepository(url)
-
-                        // Exit if wrong repository
-                        if (repository == null) {
-                            showToast(R.string.no_repository_found_error, Toast.LENGTH_LONG)
-                            return@ioSafe
-                        }
-
-                        val fixedName = if (!name.isNullOrBlank()) name
-                        else repository.name
-                        val newRepo = RepositoryData(repository.iconUrl, fixedName, url)
-                        RepositoryManager.addRepository(newRepo)
-                        extensionViewModel.loadStats()
-                        extensionViewModel.loadRepositories()
-
-                        dialog.dismissSafe(activity) // Only dismiss if the repo was added
-
-                        val plugins = RepositoryManager.getRepoPlugins(newRepo)
-                        if (plugins.isNullOrEmpty()) {
-                            showToast(R.string.no_plugins_found_error, Toast.LENGTH_LONG)
-                            return@ioSafe
-                        }
-
-                        this@ExtensionsFragment.activity?.addRepositoryDialog(
-                            newRepo
-                        )
-                    } finally {
-                        binding.applyBtt.hideProgress()
-                    }
-                }
-            }
-            binding.cancelBtt.setOnClickListener {
-                dialog.dismissSafe(activity)
-            }
         }
 
 
